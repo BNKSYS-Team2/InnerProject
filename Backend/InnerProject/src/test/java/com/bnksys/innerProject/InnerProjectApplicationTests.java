a53ea90ce6b7077911e7dc9245b4ae57bea3f9d0
추헌국 <cngjsrnr@naver.com>
2022-08-16 14:49:59 +0900

    init Backend
    자바 8
    Spring 2.7.2
    Gradle
     - spring-boot-starter-web
     - spring-boot-starter-data-jpa
     - lombok
     - spring-boot-devtools
     - h2
     - mysql

---
+++ Backend/InnerProject/src/test/java/com/bnksys/innerProject/InnerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnksys.innerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
