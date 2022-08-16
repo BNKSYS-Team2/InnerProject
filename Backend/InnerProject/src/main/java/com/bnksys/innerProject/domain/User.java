322a14962eedf144c3fdccc0ff6ca69464ee2ae0
추헌국 <cngjsrnr@naver.com>
2022-08-16 16:07:07 +0900

    add 데이터베이스 연결 및 유저 테이블 생성

---
+++ Backend/InnerProject/src/main/java/com/bnksys/innerProject/domain/User.java
@@ -0,0 +1,36 @@
+package com.bnksys.innerProject.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class User {
+	//유저번호
+	@Id
+	@Column(name="user_no")
+	@GeneratedValue
+	private long userNo;
+	
+	//유저 아이디
+	@Column(name="user_id",length = 100, nullable = false)
+	private String userId;
+	
+	//유저 비밀번호
+	@Column(name="user_password",length = 100, nullable = false)
+	private String userPassword;
+	
+	
+	//유저 이름
+	@Column(name="user_name",length = 50, nullable = false)
+	private String userName;
+		
+}
