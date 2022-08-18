5a17034d566a016e14ece52434ea76d0c561d686
김동주 <rlaehdwn23@naver.com>
2022-08-18 17:24:48 +0900

    add : header

--- bnksys-team2/src/pages/Routing.jsx
+++ bnksys-team2/src/pages/Routing.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Header from '../components/Header';
 import Login from './User/Login';
 
 export const Routing = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
