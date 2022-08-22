06f789f766d9599b9d19ec43e6be971c5bb05b8d
jjs0211 <jjs_0211_@naver.com>
2022-08-22 16:18:54 +0900

    add:pmlist

--- bnksys-team2/src/components/PM/PMList.jsx
+++ bnksys-team2/src/components/PM/PMList.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import react, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PMList.scss';
+import * as Api from '../../api';
 
 const PMList = () => {
+
+  const [data, setData] = useState({});
+  const userNo = sessionStorage.getItem('userNo');
+  
+  useEffect(() => {
+    Api.get(`/api/pm/list/${userNo}`).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <div className="container pm">
       <h1>내 저작물 목록</h1>
