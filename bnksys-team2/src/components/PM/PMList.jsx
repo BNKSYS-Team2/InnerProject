6773c6e3f4b4dfb65dc52aac7d468c3327adbda5
jjs0211 <jjs_0211_@naver.com>
2022-08-23 13:01:56 +0900

    add: mypmlist

--- bnksys-team2/src/components/PM/PMList.jsx
+++ bnksys-team2/src/components/PM/PMList.jsx
@@ -1,28 +1,30 @@
 import react, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './PMList.scss';
 import * as Api from '../../api';
 
-const PMList = async () => {
+const PMList = () => {
 
-  const [data, setData] = useState({});
+  // const [data, setData] = useState(null);
   const userNo = sessionStorage.getItem('userNo');
+  
+  const abcd = useEffect(() => {
+    getData();
+   },[]);
+   
+   const getData = async() => {
+    const res = await Api.get(`api/pm/list/${userNo}`);
+    const pmNo = res.data.pmList[0].pmNo;
+    const preview = `http://192.168.0.124:8080/api/pm/load/${userNo}/${pmNo}`;
+    // await Api.get(`api/pm/load/${userNo}/${pmNo}`);
 
-  useEffect(() => {
-    Api.get(`api/pm/list/${userNo}`).then((res) => {
-      setData(res.data);
-    });
-  }, []);
-
-  console.log(data);
-  // const pmNo = data.pmList[0].pmNo;
-
-  // const preview = await Api.get(`api/pm/load/${userNo}/${pmNo}`);
-  // console.log('preview:', preview);
+   };
   // 이 부분만 수정하면 됨
+   
   return (
-    
-    <p>{data.pmCount}</p>
+    <>
+    <img src = {'http://192.168.0.124:8080/api/pm/load/6/7'}></img>
+    <img src = {'http://192.168.0.124:8080/api/pm/load/6/7'}></img>
+    </>
   );
 };
 
