0c1b5e0beeeec7046f41adb520ca3b717d1bd956
김동주 <rlaehdwn23@naver.com>
2022-08-24 15:05:56 +0900

    add : distribute PM function

--- bnksys-team2/src/components/PM/MyPMListModal.jsx
+++ bnksys-team2/src/components/PM/MyPMListModal.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './MyPMListModal.scss';
+import * as Api from '../../api';
 
 const MyPMListModal = (props) => {
+  const [pmList, setPmList] = useState([]);
+
+  const { unit } = props;
+  const userNo = sessionStorage.getItem('userNo');
+
+  const getUserPmList = () => {
+    let utNo = unit.unitData.utNo;
+    Api.get(`api/pm/list/${userNo}/${utNo}`)
+      .then((res) => {
+        let pmArr = res.data.pmList;
+        console.log(pmArr);
+        setPmList(pmArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const selectPm = (imgUrl) => {
+    props.setSelImg(imgUrl);
+    props.setSelClick(false);
+  };
+
+  useEffect(() => {
+    getUserPmList();
+  }, []);
+
   return (
     <div className="myPMListModal">
-      {console.log(props)}
+      {console.log(unit)}
       <p className="explain">저작물을 선택해주세요.</p>
       <div className="myPMSel">
         <ul className="d-flex justify-content-center">
-          <li className="myPM">
-            <div className="pmImg"></div>
-            <p className="pmTitle">제목</p>
-          </li>
-          <li className="myPM">
-            <div className="pmImg"></div>
-            <p className="pmTitle">제목</p>
-          </li>
-          <li className="myPM">
-            <div className="pmImg"></div>
-            <p className="pmTitle">제목</p>
-          </li>
+          {pmList
+            ? pmList.map((pm) => (
+                <li className="myPM" key={pm.pmNo}>
+                  <div
+                    className="pmImg"
+                    onClick={() =>
+                      selectPm(`http://192.168.0.124:8080/api/pm/load/${userNo}/${pm.pmNo}`)
+                    }>
+                    <img
+                      src={`http://192.168.0.124:8080/api/pm/load/${userNo}/${pm.pmNo}`}
+                      alt=""
+                    />
+                  </div>
+                  <p className="pmTitle">{pm.pmTitle}</p>
+                </li>
+              ))
+            : null}
         </ul>
       </div>
       <div className="buttons">
-        <button>완료</button>
+        {/* <button>완료</button> */}
         <button onClick={() => props.setSelClick(false)}>취소</button>
       </div>
     </div>
---
+++ bnksys-team2/src/pages/PM/SelectPMList.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PMListModal from '../../components/PM/MyPMListModal.jsx';
+
+const SelectPMList = (props) => {
+  const [selClick, setSelClick] = useState(false);
+  const [selImg, setSelImg] = useState(null);
+  const { unit } = props;
+
+  const imgUrl =
+    'https://media.istockphoto.com/vectors/default-image-icon-vector-missing-picture-page-for-website-design-or-vector-id1357365823?k=20&m=1357365823&s=612x612&w=0&h=ZH0MQpeUoSHM3G2AWzc8KkGYRg4uP_kuu0Za8GFxdFc=';
+
+  return (
+    <div className="pmArea col" key={unit.id}>
+      <div className={selClick ? 'distributeWrap' : null}></div>
+      <p className="device">{unit.unitData.utName}</p>
+      <div className="pmImg d-flex justify-content-center">
+        <img src={selImg ? selImg : imgUrl} alt="" />
+      </div>
+      <button className="selBtn" onClick={() => setSelClick(true)}>
+        저작물 선택
+      </button>
+      {selClick ? (
+        <PMListModal setSelClick={setSelClick} unit={unit} setSelImg={setSelImg} />
+      ) : null}
+    </div>
+  );
+};
+
+export default SelectPMList;
