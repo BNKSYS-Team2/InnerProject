0bace6c7ec1a31cbb07b2c8028ea3d92ba84750b
추헌국 <cngjsrnr@naver.com>
2022-08-25 12:46:39 +0900

    fix 배포현황-배포지점 일괄형태로 수정

--- bnksys-team2/src/components/PM/DistributeList.jsx
+++ bnksys-team2/src/components/PM/DistributeList.jsx
@@ -36,30 +36,25 @@ const DistributeList = () => {
         
           {distributeList.map((distribute, index) => {
               return(
-                <>
-                {distribute.clients.map((client, cIndex) => {
-                  return(
-                    <>
-                      {distribute.promotionMaterials.map((promotionMaterial, pmIndex)=>{
-                        return(
-                          <tbody key = {index}>
-                            <tr>
-                              <td>{promotionMaterial.pmTitle}</td>
-                              <td>{client.company}-{client.location}-{client.utNo.utName}</td>
-                              <td>{distribute.startDt.substring(0, 4)}-{distribute.startDt.substring(4,6)}-{distribute.startDt.substring(6, 8)}-{distribute.startDt.substring(8, 10)}:00 ~ {distribute.endDt.substring(0, 4)}-{distribute.endDt.substring(4,6)}-{distribute.endDt.substring(6, 8)}-{distribute.endDt.substring(8, 10)}:00</td>
-                              <td>취소 가능</td>
-                              <td>
-                                <button className="cancelBtn1">취소</button>
-                              </td>
-                            </tr>
-                    </tbody>
-                        );
-                      })}
-                      </>
-                  );
-                })}
-
-                </>
+                  <tbody key = {index}>
+                    <tr>
+                      <td>
+                        {distribute.promotionMaterials.map((pm, pmIndex) => {
+                          return (<p key={ pmIndex}>{pm.pmTitle}</p>);
+                        })}
+                      </td>
+                      <td>
+                        {distribute.clients.map((client,clientIndex) => {
+                          return(<p key={ clientIndex}>{client.company}-{client.location}-{client.utNo.utName}</p>)
+                        }) }
+                      </td>
+                      <td>{distribute.startDt.substring(0, 4)}-{distribute.startDt.substring(4,6)}-{distribute.startDt.substring(6, 8)}-{distribute.startDt.substring(8, 10)}:00 ~ {distribute.endDt.substring(0, 4)}-{distribute.endDt.substring(4,6)}-{distribute.endDt.substring(6, 8)}-{distribute.endDt.substring(8, 10)}:00</td>
+                      <td>취소 가능</td>
+                      <td>
+                        <button className="cancelBtn1">취소</button>
+                      </td>
+                    </tr>
+                  </tbody>
               );
             })}
 
