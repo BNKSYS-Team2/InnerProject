cc26864a798d976cb66aa1989431d8cb632cff83
김동주 <rlaehdwn23@naver.com>
2022-08-26 15:22:32 +0900

    add : pm delete function

--- bnksys-team2/src/components/PM/PMList.jsx
+++ bnksys-team2/src/components/PM/PMList.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './PMList.scss';
 import * as Api from '../../api';
+import swal from 'sweetalert';
+
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const PMList = () => {
   const [imageList, setImageList] = useState([]);
@@ -21,6 +24,33 @@ const PMList = () => {
       '/create?w=' + utNo.width + '&h=' + utNo.height + '&utNo=' + utNo.utNo + '&pmNo=' + pmNo;
   };
 
+  const deletePromotionMaterial = async (userNo, pmNo) => {
+    const res = await Api.post('api/pm/delete', {
+      userNo,
+      pmNo,
+    })
+      .then((p) => {
+        if (p.data.success == 'True') {
+          swal('저작물 삭제 완료', '  ', 'success', {
+            buttons: false,
+            timer: 1200,
+          });
+          getImageData();
+        } else {
+          swal(p.data.msg, '  ', 'error', {
+            buttons: false,
+            timer: 1200,
+          });
+        }
+      })
+      .catch((error) => {
+        swal('저작물 삭제 실패', '  ', 'success', {
+          buttons: false,
+          timer: 1200,
+        });
+      });
+  };
+
   return (
     <>
       <div className="container pm">
@@ -29,17 +59,19 @@ const PMList = () => {
           {/* 이미지 카드 */}
           {imageList.map((image, index) => {
             return (
-              <div
-                className="col-4 pmBox"
-                key={index}
-                onClick={() => {
-                  clickTemplate(image.pmNo, image.utNo);
-                }}>
+              <div className="col-4 pmBox" key={index}>
                 <div className="box">
                   <img
                     key={index}
                     src={`${Api.getServerUrl()}api/pm/load/${userNo}/${image.pmNo}`}
-                    className="pmImg"></img>
+                    className="pmImg"
+                    onClick={() => {
+                      clickTemplate(image.pmNo, image.utNo);
+                    }}></img>
+                  <DeleteIcon
+                    className="deleteImg"
+                    onClick={() => deletePromotionMaterial(userNo, image.pmNo)}
+                  />
                 </div>
                 <div className="title">{image.pmTitle}</div>
               </div>
